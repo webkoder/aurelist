b5c8149c3a460d255dec64e579b9737fb677df2a
Ricardo <ricardofrr@gmail.com>
2017-10-13 01:52:19 -0300

    fix lista de caracteres

--- src/components/Caracteres.js
+++ src/components/Caracteres.js
@@ -7,7 +7,7 @@ export default class Caracteres extends Component{
         super();
         this.letras = [ 'A', 'B', 'C', 'D',
         'E', 'F', 'G', 'H', 'I', 'J', 'K',
-        'L', 'M', 'N', 'O', 'P', 'Q', 'U',
+        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
         'V', 'W', 'Y', 'X', 'Z',
         '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     }
